034ade8b4aa01eb39aa26f57051c01176c9b8754
jte91 <j3sh0n3dward5@gmail.com>
2022-07-30 19:57:25 -0500

    style the header section

--- components/Header.js
+++ components/Header.js
@@ -4,8 +4,8 @@ import { HomeIcon, UserIcon, PhoneIcon, InformationCircleIcon} from '@heroicons/
 
 export default function Header() {
     return (
-        <div>
-           <div className="">
+        <div className="bg-gray-700 text-gray-200 flex flex-col items-center p-6 select-none sm: flex-row justify-between">
+           <div className="flex">
             <HeaderIcon Icon={HomeIcon} title="HOME"/>
             <HeaderIcon Icon={UserIcon} title="ACCOUNT"/>
             <HeaderIcon Icon={PhoneIcon} title="CONTACT"/>
@@ -14,7 +14,9 @@ export default function Header() {
            <Image
            src="https://volsiz.ru/wp-content/uploads/2021/12/imdb.jpg"
            width={100}
-           height={100}/>
+           height={100}
+           className="cursor-pointer active:brightness-110"
+           />
         </div>
     )
 }
